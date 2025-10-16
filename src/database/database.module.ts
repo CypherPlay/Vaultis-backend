a5e1aa146ed66f653dd6c16bee68cefe82a58aac
Aris Hadrian <ashenarmor1013@gmail.com>
2025-10-16 04:26:03 +0000

    feat: Configure PostgreSQL database connection using TypeORM

---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Set to false in production
+        extra: {
+          max: 10, // Maximum number of connections in the pool
+          min: 2,  // Minimum number of connections in the pool
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
