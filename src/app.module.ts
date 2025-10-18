74574a34a4f6db37019ec114814ffb2640f4c6c7
Kenneth Nelson <kenneth.nelson.dev@gmail.com>
2025-10-18 03:33:28 -0600

    feat: Implement JWT authentication module (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from './database/database.module';
 import { RiddleModule } from './riddle/riddle.module';
 import { GuessModule } from './guess/guess.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, RiddleModule, GuessModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    RiddleModule,
+    GuessModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
