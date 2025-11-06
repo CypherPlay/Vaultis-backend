fc5ee5c79cbb22cfe236bccd4011457ee1f1ccc8
Henrik Magnus Voss <henrik.voss47@gmail.com>
2025-11-06 13:48:01 -0700

    Implement Atomic Guess Submission with Database Transaction (#25)
    * Implement transactional guess submission
    
    * Fix: Ensure deductEntryFee runs within a transaction

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from './database/database.module';
 import { RiddleModule } from './riddle/riddle.module';
 import { WalletModule } from './wallet/wallet.module';
+import { AuthModule } from './auth/auth.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
 
 @Module({
   imports: [AuthModule, DatabaseModule, GuessesModule, LeaderboardModule, RiddleModule, WalletModule],
